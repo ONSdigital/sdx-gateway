e32997adbcc218c38573dece50af7396b1c7f6aa
Warren Bailey <warren@warrenbailey.net>
2017-11-06 11:39:40 +0000

    Configurable queues

--- app/settings.py
+++ app/settings.py
@@ -1,11 +1,11 @@
 import os
 
-COLLECT_QUEUE = 'sdx_gateway_collect'
+COLLECT_QUEUE = os.getenv('RABBIT_SURVEY_QUEUE', 'sdx_gateway_collect')
 DEFAULT_USER = os.getenv('SDX_GATEWAY_DEFAULT_USER', 'rabbit')
 DEFAULT_PASSWORD = os.getenv('SDX_GATEWAY_DEFAULT_PASSWORD', 'rabbit')
 QUARANTINE_QUEUE = 'bridge_quarantine'
 RABBIT_EXCHANGE = 'exchange'
-EQ_QUEUE = 'submit_queue'
+EQ_QUEUE = os.getenv('RABBIT_GATEWAY_QUEUE', 'survey')
 RABBIT_HOST = os.getenv('RABBITMQ_HOST', '0.0.0.0')
 RABBIT_HOST2 = os.getenv('RABBITMQ_HOST2', '0.0.0.0')
 RABBIT_PORT = '5672'
