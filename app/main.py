a3152becf2b6dd900507a331b5225924a00073b0
Adam Mann <manny160389@hotmail.com>
2019-07-12 11:42:27 +0100

    Remove healthcheck

--- app/main.py
+++ app/main.py
@@ -130,10 +130,6 @@ class GetHealth:
                 self._default_user,
                 self._default_pass,
                 self._rabbit_hosts[1]),
-            'http://{}:{}@{}:15672/api/healthchecks/node'.format(
-                settings.SDX_GATEWAY_SDX_RABBITMQ_USER,
-                settings.SDX_GATEWAY_SDX_RABBITMQ_PASSWORD,
-                settings.SDX_GATEWAY_SDX_RABBITMQ_HOST),
         ]
 
     @gen.coroutine
